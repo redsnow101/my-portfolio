13b7a0f09447cdeca265466e353b32c21deab8ac
redsnow101 <rabindrathapa.dev@gmail.com>
2025-07-06 13:13:50 +0530

    removed unused

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -4,11 +4,11 @@ function NavBar() {
     <div className='flex flex-row justify-between m-2  w-full '>
         <p className="font-mono md:text-2xl lg:text-3xl">RABINDRA THAPA</p>
 
-        <div className="flex mr-1 hidden sm:block ">
+        {/* <div className="flex mr-1 hidden sm:block ">
             <a href="#" className="mr-1">home</a>
             <a href="#" className="mr-1">about</a>
             <a href="#" className="mr-1">contact</a>
-        </div>
+        </div> */}
         
 
     </div>
